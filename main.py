595c61eee9b14d0791b81e8e9cbabbeae908e3bd
root <root@localhost.localdomain>
2019-11-02 10:19:30 +0100

    in order

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 from langues import *
+from math import *
 
+#my text
 var_a = "hello world"
 
-#dictionnaire de la table d'occurance
+#dictionnaire de la table d'occurance utilise par de_str_a_table_doccurance(textLocal)
 dic1 = {}
 
-#dictionnaire de la table de freqeuce
+#dictionnaire de la table de freqeuce utlise par de_str_a_table_de_frequece(localDic)
 dic2 = {}
 
 #get keys from dictionnaire
 def getList(dict):
     return list(dict.keys())
-
 
 #calculer le nombre total des caractere contient un dictionnaire
 def lenOfDic(localDic):
@@ -21,11 +22,9 @@ def lenOfDic(localDic):
         len += localDic[keys]
     return len
 
-
-
 #table d'occurance
-def de_str_a_table_doccurance():
-    for element in var_a:
+def de_str_a_table_doccurance(textLocal):
+    for element in textLocal:
         if element in getList(dic1) :
             dic1[element] += 1
         else:
@@ -38,11 +37,6 @@ def de_str_a_table_de_frequece(localDic):
         dic2[value] /= lenOfDic(dic1)
 
 
-de_str_a_table_doccurance()
-de_str_a_table_de_frequece(dic1)
-
-print(dic2)
-
 
 #fonction permet de calculer la distance entre la langue ecrit par utilisateur et
 #langue definie dans la fichier langues.py
@@ -51,3 +45,12 @@ print(dic2)
     #calcule
 #sinon
     #fo = 0 et calcule
+
+
+
+
+
+de_str_a_table_doccurance(var_a)
+de_str_a_table_de_frequece(dic1)
+
+print(dic1)
